bf505359fba170259dca07be9e152429b49bc661
Origami5822 <sodamm2kp@gmail.com>
2025-09-17 16:51:13 +0700

    add RGB

--- game.py
+++ game.py
@@ -6,11 +6,23 @@ pygame.init()
 ### กำหนดข้อความที่ Titlebar
 pygame.display.set_caption("just game")
 
-### สร้างหน้าจอเกม กว้าง สูง
-screen = pygame.display.set_mode((640,360))
+### ตั้งสี RGB
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+Blue = (0,0,255)
+
+### กำหนดหน้าจอ
+SCREEN_W = 600
+SCREEN_H = 300 
+screen = pygame.display.set_mode((SCREEN_W,SCREEN_H))
+
+### แสดงหน้าจอ
+screen.fill(WHITE)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.update()
 pygame.quit() ### ออกเกม
